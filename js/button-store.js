13c3df585a9fd86b7ee42344de085183f342607b
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2019-02-12 02:55:05 +0000

    Desafio Avançado

--- js/button-store.js
+++ js/button-store.js
@@ -1,7 +1,8 @@
 const $second = document.querySelector(".-second");
+const $carinho = document.querySelector(".header-store .action.-last");
 
 $second.addEventListener("click", handleClick);
 
 function handleClick () {
-  console.log("top - button");
+  $carinho.textContent = "Clicado";
 }
