bf8ee3d1fbf9b79997e3bdac84c5f96c82fddf9f
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2019-02-14 19:39:21 +0000

    Desafio Intermediário

--- js/description-product.js
+++ js/description-product.js
@@ -1,6 +1,22 @@
 const $heart = window.document.querySelector(".-heart");
+const $classification = document.querySelectorAll(".classification");
+console.log($classification);
+
+$classification.forEach(star => {
+  console.log(star);
+});
 
 $heart.addEventListener("click", handleClick);
+
+$classification.forEach(star => {
+  star.addEventListener("click", () => {
+    if (star.src == "http://127.0.0.1:5500/img/star-active.png"){
+       star.src = "img/star.png";
+    } else {
+      star.src = "img/star-active.png";
+    }
+  });
+});
 
 function handleClick(){
   $heart.classList.toggle("-active");
