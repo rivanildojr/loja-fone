76587da85dab62bc61fcfe9cf949de56e51750e6
rivanildojr <rivanildo_junior7@hotmail.com>
2020-06-11 16:32:32 -0300

    finalizado o input-search

---
+++ js/input-search.js
@@ -0,0 +1,17 @@
+const $inputSearch = document.querySelector(".input-search");
+const $open = $inputSearch.querySelector(".-open");
+const $close = $inputSearch.querySelector(".-close");
+
+$open.addEventListener("click", (event) => {
+  event.preventDefault();
+  $inputSearch.classList.toggle("-active");
+  $open.classList.remove("-active");
+  $close.classList.add("-active");
+});
+
+$close.addEventListener("click", (event) => {
+  event.preventDefault();
+  $inputSearch.classList.toggle("-active");
+  $open.classList.add("-active");
+  $close.classList.remove("-active");
+});
